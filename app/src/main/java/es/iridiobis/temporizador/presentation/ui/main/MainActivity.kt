f233b6786aa372ac7e92688d72578039c24bc3a9
Jose Luis Benito Esteban <iridiobis@gmail.com>
2017-02-19 20:24:55 +0100

    mvp for main

--- app/src/main/java/es/iridiobis/temporizador/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/es/iridiobis/temporizador/presentation/ui/main/MainActivity.kt
@@ -3,23 +3,38 @@ package es.iridiobis.temporizador.presentation.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import es.iridiobis.kotlinexample.TasksAdapter
 import es.iridiobis.kotlinexample.snack
-import es.iridiobis.kotlinexample.startActivity2
 import es.iridiobis.temporizador.R
 import es.iridiobis.temporizador.core.alarm.AlarmHandler
 import es.iridiobis.temporizador.data.storage.ImagesStorage
 import es.iridiobis.temporizador.data.storage.TasksStorage
 import es.iridiobis.temporizador.domain.model.Task
 import es.iridiobis.temporizador.presentation.ui.runningtask.RunningTaskActivity
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Main.View {
+
+    val presenter = MainPresenter(TasksStorage())
+    val tasksAdapter = TasksAdapter(ImagesStorage()) { startTask(it) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        main_tasks.adapter = tasksAdapter
+        presenter.attach(this)
+
+        fab.setOnClickListener { view ->
+            view.snack("Replace with your own action")
+        }
+    }
+
+    override fun displayTasks(tasks: List<Task>) {
+        tasksAdapter.data = tasks
+    }
 
     private fun startTask(task: Task) {
         main_tasks.snack("Starting task " + task.name)
@@ -29,44 +44,5 @@ class MainActivity : AppCompatActivity() {
                         .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                         .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         )
-    }
-
-    val tasksAdapter = TasksAdapter(ImagesStorage()) { startTask(it) }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        main_tasks.adapter = tasksAdapter
-        //TODO mvp, injection
-        TasksStorage().retrieveTasks()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.io())
-                .subscribe({ tasksAdapter.data = it })
-
-        fab.setOnClickListener { view ->
-            view.snack("Replace with your own action")
-        }
     }
 }
