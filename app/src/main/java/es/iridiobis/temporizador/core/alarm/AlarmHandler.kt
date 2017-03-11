d7773a1e73a293b0a09cb7de36b8700ab0d51a85
Jose Luis Benito Esteban <iridiobis@gmail.com>
2017-03-11 10:32:17 +0100

    Centralise notification creation

--- app/src/main/java/es/iridiobis/temporizador/core/alarm/AlarmHandler.kt
+++ app/src/main/java/es/iridiobis/temporizador/core/alarm/AlarmHandler.kt
@@ -7,19 +7,16 @@ import android.content.Context
 import android.os.Build
 import android.os.SystemClock
 import android.preference.PreferenceManager
-import android.provider.MediaStore
-import android.support.v4.app.NotificationCompat
-import es.iridiobis.temporizador.R
+import es.iridiobis.temporizador.core.notification.NotificationProvider
 import es.iridiobis.temporizador.data.storage.TasksStorage
 import es.iridiobis.temporizador.domain.model.Task
 import es.iridiobis.temporizador.domain.services.AlarmService
 import es.iridiobis.temporizador.presentation.services.AlarmMediaService
-import es.iridiobis.temporizador.presentation.ui.runningtask.RunningTaskActivity
 import io.reactivex.Observable
 import javax.inject.Inject
 
-class AlarmHandler @Inject constructor(val tasksStorage: TasksStorage, val context : Context) : AlarmService {
-    var task : Task? = null
+class AlarmHandler @Inject constructor(val tasksStorage: TasksStorage, val notificationProvider: NotificationProvider, val context: Context) : AlarmService {
+    var task: Task? = null
     override fun getRunningTask(): Observable<Task?> {
         if (task != null) {
             return Observable.just(task)
@@ -40,40 +37,9 @@ class AlarmHandler @Inject constructor(val tasksStorage: TasksStorage, val conte
             alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + task.duration, alarmIntent)
         else
             alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + task.duration, alarmIntent)
-        showRunningNotification(task)
-    }
-
-    private fun showRunningNotification(it: Task) {
-
-        val pendingPause = PendingIntent.getBroadcast(context, 0, AlarmReceiver.stopIntent(context), PendingIntent.FLAG_CANCEL_CURRENT)
-
-        val pause = NotificationCompat.Action(
-                R.drawable.ic_notifications_off_black_24,
-                context.getString(R.string.pause),
-                pendingPause)
-
-        val content = PendingIntent.getActivity(context, 0, RunningTaskActivity.newIntent(it.id, context), 0)
-
-        val builder = getBaseNotificationBuilder(it)
-                .setContentText("Keep working")
-                .setContentIntent(content)
-                .addAction(pause)
 
         val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.notify(AlarmMediaService.ID_NOTIFICATION, builder.build())
+        notificationManager.notify(notificationProvider.notificationId, notificationProvider.showRunningNotification(task))
     }
-
-    private fun getBaseNotificationBuilder(it: Task): NotificationCompat.Builder {
-        return NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.ic_task_add_white_24)
-                .setLargeIcon(MediaStore.Images.Media.getBitmap(context.contentResolver, it.thumbnail))
-                .setContentTitle(it.name)
-                .setShowWhen(false)
-                .setAutoCancel(false)
-                .setOngoing(true)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setCategory(NotificationCompat.CATEGORY_ALARM)
-    }
-
+    
 }
--- app/src/main/java/es/iridiobis/temporizador/presentation/services/AlarmMediaService.kt
+++ app/src/main/java/es/iridiobis/temporizador/presentation/services/AlarmMediaService.kt
@@ -1,25 +1,15 @@
 package es.iridiobis.temporizador.presentation.services
 
-import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
 import android.media.RingtoneManager
 import android.os.IBinder
 import android.os.PowerManager
-import android.preference.PreferenceManager
-import android.provider.MediaStore
-import android.support.v4.app.NotificationCompat
-import es.iridiobis.temporizador.R
 import es.iridiobis.temporizador.core.ApplicationComponent
-import es.iridiobis.temporizador.core.alarm.AlarmReceiver
 import es.iridiobis.temporizador.core.di.ComponentProvider
-import es.iridiobis.temporizador.data.storage.ImagesStorage
-import es.iridiobis.temporizador.data.storage.TasksStorage
-import es.iridiobis.temporizador.domain.model.Task
+import es.iridiobis.temporizador.core.notification.NotificationProvider
 import es.iridiobis.temporizador.domain.services.AlarmService
-import es.iridiobis.temporizador.presentation.ui.finishedtask.FinishedTaskActivity
-import es.iridiobis.temporizador.presentation.ui.runningtask.RunningTaskActivity
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import java.io.IOException
@@ -29,13 +19,13 @@ class AlarmMediaService : Service(), MediaPlayer.OnPreparedListener,
         MediaPlayer.OnErrorListener {
 
     companion object {
-        val ID_NOTIFICATION = 1
         val ACTION_PLAY = "es.iridiobis.temporizador.presentation.services.ACTION_PLAY"
         val ACTION_PAUSE = "es.iridiobis.temporizador.presentation.services.ACTION_PAUSE"
         val ACTION_STOP = "es.iridiobis.temporizador.presentation.services.ACTION_STOP"
     }
 
     @Inject lateinit var alarmService: AlarmService
+    @Inject lateinit var notificationProvider: NotificationProvider
 
     var mediaPlayer: MediaPlayer? = null
 
@@ -54,7 +44,7 @@ class AlarmMediaService : Service(), MediaPlayer.OnPreparedListener,
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribeOn(Schedulers.io())
                     .subscribe({
-                        showFinishedNotification(it!!)
+                        startForeground(notificationProvider.notificationId, notificationProvider.showFinishedNotification(it!!))
                     })
             initMediaPlayer()
         }
@@ -104,35 +94,4 @@ class AlarmMediaService : Service(), MediaPlayer.OnPreparedListener,
         return true
     }
 
-    private fun showFinishedNotification(it: Task) {
-
-        val pendingSnooze = PendingIntent.getBroadcast (this, 0, AlarmReceiver.stopIntent(this), PendingIntent.FLAG_CANCEL_CURRENT)
-
-        val dismiss = NotificationCompat.Action(
-                R.drawable.ic_notifications_off_black_24,
-                getString(R.string.done),
-                pendingSnooze)
-
-        val content = PendingIntent.getActivity (this, 0, FinishedTaskActivity.newIntent(it.id, this), 0)
-
-        val builder = getBaseNotificationBuilder(it)
-                .setContentText(getString(R.string.enough_message))
-                .setContentIntent(content)
-                .addAction(dismiss)
-
-        startForeground(ID_NOTIFICATION, builder.build())
-    }
-
-    private fun getBaseNotificationBuilder(it: Task) : NotificationCompat.Builder {
-        return NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_task_add_white_24)
-                .setLargeIcon(MediaStore.Images.Media.getBitmap(contentResolver, it.thumbnail))
-                .setContentTitle(it.name)
-                .setShowWhen(false)
-                .setAutoCancel(false)
-                .setOngoing(true)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .setPriority(NotificationCompat.PRIORITY_MAX)
-                .setCategory(NotificationCompat.CATEGORY_ALARM)
-    }
 }
